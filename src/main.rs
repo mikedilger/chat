fb1a931ed96d1855f6f94b6e941a8036fbc32b70
Mike Dilger <mike@mikedilger.com>
2015-09-20 11:42:26 +1200

    mimimal usage of mio

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 
 extern crate mio;
 
+use mio::{EventLoop,Handler};
+
+struct EventHandler;
+
+impl Handler for EventHandler {
+    type Timeout = ();
+    type Message = ();
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Create the event loop
+    let mut event_loop: EventLoop<EventHandler> = EventLoop::new().unwrap();
+
+    // Create the event handler
+    let mut event_handler = EventHandler;
+
+    // Run the event loop
+    event_loop.run(&mut event_handler).unwrap();
 }
