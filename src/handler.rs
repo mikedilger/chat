da04f1f47872fb3675f48a3bfaa225956fc78dbc
Mike Dilger <mike@mikedilger.com>
2015-09-20 12:34:02 +1200

    Handle server-listener readable events by accepting clients

--- src/handler.rs
+++ src/handler.rs
@@ -1,14 +1,9 @@
 
-use mio::{Handler,EventLoop};
-use server::Server;
+use mio::{Handler,EventLoop,Token,EventSet};
+use server::{Server,LISTENER_FD};
 
 pub struct EventHandler {
     server: Server,
-}
-
-impl Handler for EventHandler {
-    type Timeout = ();
-    type Message = ();
 }
 
 impl EventHandler {
@@ -20,5 +15,21 @@ impl EventHandler {
 
     pub fn register_server(&mut self, event_loop: &mut EventLoop<EventHandler>) {
         self.server.register(event_loop);
+    }
+}
+
+impl Handler for EventHandler {
+    type Timeout = ();
+    type Message = ();
+
+    fn ready(&mut self, event_loop: &mut EventLoop<EventHandler>,
+             token: Token, _events: EventSet)
+    {
+        match token {
+            LISTENER_FD => {
+                self.server.accept(event_loop);
+            },
+            _ => unreachable!("Ready event for unknown token"),
+        }
     }
 }
